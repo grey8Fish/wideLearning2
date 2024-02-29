9ae9b56a00efa9863bb56e9cac446ba707943e3b
grey8fish <fentazer@mail.ru>
2024-02-29 19:34:27 +0500

    В классе wideLearningSerialLayer приравнены columnName = None и self.classesName = None

--- cSingsSummTargetOpposit/models/wideLearningSerialLayer.py
+++ cSingsSummTargetOpposit/models/wideLearningSerialLayer.py
@@ -3,33 +3,16 @@ import csv
 from models.DataLoader import DataLoader
 
 class wideLearningSerialLayer:
-	def __init__(self, countClasses, maxInstance, sizeVector, nameFile):
-	    #получает количество столбцов=размер вектора весов, количество классов,
-        #количество экземпляров, имя файла
-		
-		self.currentWeights = np.zeros(sizeVector, dtype=int)
-		#текущий вектор весов
-
-		self.previousWeights = np.zeros(sizeVector, dtype=int)
-		#предыдущий вектор весов
-
-		#self.bestWeights = np.zeros(10, sizeVector+1, dtype=int)
-		#10 лучших весов, в последнем столбце количество отсеченных
-		
-		self.countInstancesEachClassCorrection = np.zeros(countClasses, dtype=int)
-		#количество экземпляров в каждом классе корректирующей выборки
-
-		self.countInstancesEachClassTraining = np.zeros(countClasses, dtype=int)
-		#количество экземпляров в каждом классе обучающей выборки
-		
-		#self.classesName
-		#имена классов
-
-		self.inputsClassCorrection = np.zeros((countClasses, maxInstance, sizeVector), dtype=int)
-		#входные экземпляры корректирующей выборки
-		
-		self.inputsClassTraining = np.zeros((countClasses, maxInstance, sizeVector), dtype=int)
-		#входные экземпляры обучающей выборки
+	def __init__(self, countClasses, maxInstance, sizeVector, nameFile):	#получает количество столбцов=размер вектора весов, количество классов, количество экземпляров, имя файла
+		self.currentWeights = np.zeros(sizeVector, dtype=int)		#текущий вектор весов
+		self.previousWeights = np.zeros(sizeVector, dtype=int)		#предыдущий вектор весов
+		#self.bestWeights = np.zeros(10, sizeVector+1, dtype=int)	#10 лучших весов, в последнем столбце количество отсеченных
+		self.countInstancesEachClassCorrection = np.zeros(countClasses, dtype=int)	#количество экземпляров в каждом классе корректирующей выборки
+		self.countInstancesEachClassTraining = np.zeros(countClasses, dtype=int)	#количество экземпляров в каждом классе обучающей выборки
+		self.columnName = None		#имена столбцов
+		self.classesName = None		#имена классов
+		self.inputsClassCorrection = np.zeros((countClasses, maxInstance, sizeVector), dtype=int)	#входные экземпляры корректирующей выборки
+		self.inputsClassTraining = np.zeros((countClasses, maxInstance, sizeVector), dtype=int)		#входные экземпляры обучающей выборки
 
 	def setColumnName(self, columName):
 		self.columnName = columName #имена столбцов
