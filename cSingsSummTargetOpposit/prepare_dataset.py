4fd702fed2dedc38eecb43b7819749616801e5d8
ed <18447588+cyclomethane@users.noreply.github.com>
2024-03-06 16:05:02 +0500

    Исправление вызова для ignored_columns

--- cSingsSummTargetOpposit/prepare_dataset.py
+++ cSingsSummTargetOpposit/prepare_dataset.py
@@ -194,20 +194,19 @@ def process(file_name, class_column, instance_column=None, excluded_columns=None
 
 
 if __name__ == "__main__":
-    
-    # Меняем параметры только в этом болке
     file_name = "loan_sanction_test.csv"
-    class_column = "Married" # Целевая колонка
-    instance_column = "Loan_ID" # ID колонка (если есть)
-    excluded_columns = []  # Список колонок, которые будут исключены из обработки (если есть)
+    class_column = "Married"  # Целевая колонка
+    instance_column = "Loan_ID"  # ID колонка (если есть)
+    excluded_columns = []  # Список колонок, которые будут ИСКЛЮЧЕНЫ из выборки (если необходимо) - данных колонок НЕ будет в выходном файле
+    ignored_columns = []  # Список колонок, которые будут ИГНОРИРОВАТЬСЯ обработчиком (если есть) - данные колонки будут в выходном файле, но не будут преобразованы
 
     # Создание словаря с аргументами для функции
     process_args = {
         "file_name": file_name,
         "class_column": class_column,
-        # Добавляем instance_column и excluded_columns только если они определены и не пусты
-        **({"instance_column": "Loan_ID"} if 'instance_column' in locals() else {}),  # Условное добавление instance_column
+        **({"instance_column": instance_column} if instance_column else {}),  # Условное добавление instance_column
         **({"excluded_columns": excluded_columns} if excluded_columns else {}),  # Условное добавление excluded_columns
+        **({"ignored_columns": ignored_columns} if ignored_columns else {}),  # Условное добавление ignored_columns
     }
 
     # Вызов функции с использованием распаковки словаря аргументов
