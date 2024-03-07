91200bec77810bb4c002ef58bba1045306729d16
ed <18447588+cyclomethane@users.noreply.github.com>
2024-03-07 16:54:24 +0500

    Исправлен def print_arg_classes(self):

--- cSingsSummTargetOpposit/models/DataLoader.py
+++ cSingsSummTargetOpposit/models/DataLoader.py
@@ -1,9 +1,5 @@
 ﻿#1. Пробный шаг надо сделать первым шагом
 #2. Вставить проверку противоположной категории
-
-#Нужно в argClasses исправить вывод и данные - перва строка должна быть входными данными
-#Вывод numpy массивом
-
 import csv
 import numpy as np
 import pandas as pd
@@ -31,7 +27,7 @@ class DataLoader:
         self.file_names = file_names  # Список имен файлов CSV для обработки
         self.classes_count = 0        # Количество уникальных классов в данных
         self.instances_max = 0        # Максимальное количество экземпляров в каждом классе
-        self.ordinate_count = 0       # Количество столбцов в данных за исключением 'number' и 'target'
+        self.ordinate_count = 0       # Количество столбцов в данных за исключением 'RowNum' и target_column
         self.arg_classes = None       # Массив для хранения обработанных данных
         self.column_names = None      # Список названий столбцов
         self.target_column = None     # Имя самой правой колонки
@@ -51,8 +47,8 @@ class DataLoader:
                 if not self.column_names:
                     # Задание имени самой правой колонки
                     self.target_column = csv_reader.fieldnames[-1]
-                    # Исключаем 'number' и самую правую колонку, имя которой хранится в self.target_column
-                    self.column_names = [col for col in csv_reader.fieldnames if col not in ['number', 'ID', self.target_column]]
+                    # Исключаем 'RowNum' и самую правую колонку, имя которой хранится в self.target_column
+                    self.column_names = [col for col in csv_reader.fieldnames if col not in ['RowNum', self.target_column]]
                     self.ordinate_count = len(self.column_names)
                     #print(len(self.column_names))
 
@@ -143,22 +139,33 @@ class DataLoader:
         """
         Вывод данных arg_classes в виде таблицы.
         """
-        # Преобразуем данные из self.arg_classes в список списков для DataFrame
-        data_list = self.arg_classes.reshape(-1, self.arg_classes.shape[-1])
-        # Удаляем строки, полностью состоящие из нулей
-        data_list = data_list[~np.all(data_list == 0, axis=1)]
-
-        # Формируем полный список названий столбцов, включая специальные столбцы 'number' и 'target'
-        # Учитываем, что self.column_names уже содержит нужные названия столбцов без 'number' и 'target'
-        full_column_names = self.column_names + ['number', 'target']
-
-        # Создаем DataFrame только с необходимыми столбцами
-        # Ограничиваем количество столбцов в DataFrame до количества в full_column_names
-        df = pd.DataFrame(data_list[:, :len(full_column_names)], columns=full_column_names)
-
-        # Выводим DataFrame с учетом настроек отображения
-        with pd.option_context('display.max_rows', None, 'display.max_columns', None):
-            print(df.to_string(index=False))
+        # Предполагаем, что есть 4 дополнительных столбца (например, 'Extra')
+        total_columns = self.ordinate_count + 4
+    
+        # Проходим по каждому классу
+        for class_index in range(self.classes_count):
+            class_name = self.class_names[class_index]
+            print(f"\nClass {class_name}:")
+        
+            # Проходим по каждому экземпляру в классе
+            for instance_index in range(self.instances_max):
+                instance_data = []
+                # Проходим по каждому атрибуту 
+                for attribute_index in range(self.ordinate_count):
+                    # Получаем значение атрибута
+                    value = self.arg_classes[class_index, instance_index, attribute_index]
+                    # Формируем строку атрибут-значение
+                    attribute_value_str = f"{self.column_names[attribute_index]}: {value}"
+                    instance_data.append(attribute_value_str)
+            
+                # Добавляем дополнительные столбцы, если они есть
+                for extra_column_index in range(self.ordinate_count, total_columns):
+                    extra_value = self.arg_classes[class_index, instance_index, extra_column_index]
+                    if extra_value != 0:  # Предположим, что выводим дополнительные столбцы, только если они не нулевые
+                        instance_data.append(f"Extra{extra_column_index-self.ordinate_count+1}: {extra_value}")
+            
+                # Выводим данные экземпляра
+                print(f"Instance {instance_index+1}: " + ", ".join(instance_data))
 
 
     def get_class_names(self):
